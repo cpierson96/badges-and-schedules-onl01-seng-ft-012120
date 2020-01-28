5b574f08977015c52c1dc028b047543e46ca64b1
Claudia Pierson <c172@umbc.edu>
2020-01-28 05:01:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
-attendee= ["Edsger","Ada","Charles,""Alan","Grace","Linus", "Matz"]
 
 def batch_badge_creator(name)
- result=[]
  "Hello, my name is #{name}."
 end 
 
-attendee.each do |name|
-  puts batch_badge_creator(name)
-end
+#attendee.each do |name|
+ # puts batch_badge_creator(name)
+#end
