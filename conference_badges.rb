339e45d38a67f5d0ce98d341c6816d087391a4cf
Claudia Pierson <c172@umbc.edu>
2020-01-28 06:19:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,15 @@
 # Write your code here.
 
+
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  result = []
-  attendees.each do |badge|
-    result.push(batch_maker)
-end  
+def batch_badge_creator(names)
+  greetings = [] # initialize greetings as an empty array
+  names.each do |name| # for each name in the names array
+    greetings <<  badge_maker(name)# add a greeting for that name
+  end
+  return greetings # return the array of all greetings, at the end
 end
-    return result
-    end
+  
